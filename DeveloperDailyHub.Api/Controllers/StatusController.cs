b76d5687bfdcdad9cc5d90e3145e43e3d0cbd51c
Steven Scoville <s.scoville.dev@outlook.com>
2026-02-19 09:08:48 -0600

    feat: add GET /api/status endpoint returning application metadata

---
+++ DeveloperDailyHub.Api/Controllers/StatusController.cs
@@ -0,0 +1,21 @@
+﻿using DeveloperDailyHub.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeveloperDailyHub.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<StatusResponse> GetStatus()
+        {
+            return Ok(new StatusResponse
+            {
+                Name = "DeveloperDailyHub API",
+                Version = "v1.0",
+                ServerTime = DateTime.UtcNow
+            });
+        }
+    }
+}
---
+++ DeveloperDailyHub.Api/Models/StatusResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace DeveloperDailyHub.Api.Models
+{
+    public class StatusResponse
+    {
+        public string Name { get; set; } = "";
+        public string Version { get; set; } = "";
+        public DateTime ServerTime { get; set; }
+    }
+}
