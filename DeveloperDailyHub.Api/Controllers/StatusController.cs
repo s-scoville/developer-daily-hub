7fc5ba381cf18e72f7db7aea78ce982e63dd1849
Steven Scoville <s.scoville.dev@outlook.com>
2026-02-19 08:37:46 -0600

    lab: implement status endpoint and validate routing

---
+++ DeveloperDailyHub.Api/Controllers/StatusController.cs
@@ -0,0 +1,24 @@
+﻿using DeveloperDailyHub.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeveloperDailyHub.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<StatusResponse> GetStatus()
+        {
+            return Ok(new StatusResponse
+            {
+                Name = "Developer Daily Hub",
+                Version = "1.0",
+                ServerTime = DateTime.UtcNow
+            });
+
+            return BadRequest("Forced failure");
+        }
+    }
+}
