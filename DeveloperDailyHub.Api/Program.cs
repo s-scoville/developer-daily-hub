d8eafe757f9b6c8e2911d594ed397dabc44a46da
Steven Scoville <s.scoville.dev@outlook.com>
2026-02-20 10:19:58 -0600

    lab: add status integration test and suppress https redirect during test runs

--- DeveloperDailyHub.Api/Program.cs
+++ DeveloperDailyHub.Api/Program.cs
@@ -21,7 +21,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsEnvironment("Testing"))
+{ 
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthorization();
 
---
+++ DeveloperDailyHub.ApiTests/StatusEndpointTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+
+namespace DeveloperDailyHub.ApiTests
+{
+    internal class StatusEndpointTests
+    {
+        [Test]
+        public async Task Get_Status_Returns_Ok_And_Expected_Fields()
+        {
+            // Arrange
+            await using var factory = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder => builder.UseEnvironment("Testing"));
+            using var client = factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/api/status");
+            var json = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            using var doc = JsonDocument.Parse(json);
+            var root = doc.RootElement;
+
+            Assert.That(root.TryGetProperty("name", out _), Is.True);
+            Assert.That(root.TryGetProperty("version", out _), Is.True);
+            Assert.That(root.TryGetProperty("serverTime", out _), Is.True);
+        }
+    }
+}
